210c7257ac65256f71b22c798c5bbc7bd2a18050
Seamus Lee <seamuslee001@gmail.com>
2016-11-30 01:13:11 +0000

    Fix comment

--- multisite.php
+++ multisite.php
@@ -209,7 +209,7 @@ function multisite_civicrm_aclGroup($type, $contactID, $tableName, &$allGroups, 
   }
   if(!empty($allGroups)) {
     //all groups is empty if we really mean all groups but if a filter like 'is_disabled' is already applied
-    // it is populated, ajax calls from Manage Groups will leave empty but calls from New Mailing pass in a filtered listi
+    // it is populated, ajax calls from Manage Groups will leave empty but calls from New Mailing pass in a filtered list
     $originalCurrentGroups = $currentGroups;
     $currentGroups = array_intersect($currentGroups, array_flip($allGroups));
     $currentGroups = array_merge($currentGroups, array_intersect($originalCurrentGroups, $disabledGroups));
