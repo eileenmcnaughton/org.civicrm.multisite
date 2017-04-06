ab05c8eac0688e03ae73a5655dccb36666f63e32
Seamus Lee <seamuslee001@gmail.com>
2017-04-07 08:37:13 +1000

    Implement hook_civicrm_selectWhereClause for groups and use apiWrappers to limit mailings lookup to current domain

---
+++ CRM/Multisite/MailingWrapper.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Wrapper class to restrict Mailing API calls to the current domain.
+ */
+class CRM_Multisite_MailingWrapper implements API_Wrapper {
+
+  /**
+   * Handle API Input
+   * @param array $apiRequest
+   */
+  public function fromApiInput($apiRequest) {
+    if (empty($apiRequest['params']['params']['domain_id'])) {
+      $apiRequest['params']['params']['domain_id'] = CRM_Core_Config::domainID();
+    }
+
+    return $apiRequest;
+  }
+
+  /**
+   * Handle returning results from API request
+   * @param array $apiRequest
+   * @param array $result
+   * @return array api result
+   */
+  public function toApiOutput($apiRequest, $result) {
+    return $result;
+  }
+
+}
