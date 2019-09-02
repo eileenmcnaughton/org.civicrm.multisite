1586c484e052b6dd23cb49da3595bb8cd8b941e2
Seamus Lee <seamuslee001@gmail.com>
2019-09-03 07:00:46 +1000

    Fix syntax errors

--- multisite.php
+++ multisite.php
@@ -655,11 +655,11 @@ function multisite_civicrm_alterEntityRefParams(&$props = [], $formName) {
         $props['api']['params']['domain_id'] = NULL;
       }
       else {
-        $props['api']['params'] = ['domain_id' => NULL]];
+        $props['api']['params'] = ['domain_id' => NULL];
       }
     }
     else {
-      $props['api'] = ['params' => ['domain_id' => NULL]];
+      $props['api'] = ['params' => ['domain_id' => NULL];
     }
   }
 }
